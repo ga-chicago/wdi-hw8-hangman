c8ef90d5f19daaf17821a188a398e91aa1790271
Anthony <anthonyjlower@gmail.com>
2017-12-20 19:29:13 -0600

    created game object with pseudo code

--- js/app.js
+++ js/app.js
@@ -3,9 +3,33 @@
 const words = ['hello', 'potato', 'seven', 'tree', 'apple'];
 
 function chooseWords() {
-	console.log(words[Math.floor(Math.random()*words.length)])
+	let chosenWord = words[Math.floor(Math.random()*words.length)]
 }
 
+
+game = {
+	//guesses remaining
+	guesses: 10,
+	//guessed letter
+	gussedLetters: [],
+	isOver() {
+		// Check if game has been won
+
+		//Check if game has been lost
+
+		//if neither do this
+
+
+	},
+	overMessage() {
+		//If game has been won return "You Win"
+
+		//If game has been lost return "You Lose"
+
+
+	}
+
+}
 
 
 chooseWords()
