2a9d4c69b8b5c7534f63ddf02bc70042bdecefcf
Anthony <anthonyjlower@gmail.com>
2017-12-20 20:06:42 -0600

    Completed pseudo code for word and letter classes

--- js/app.js
+++ js/app.js
@@ -2,34 +2,81 @@
 
 const words = ['hello', 'potato', 'seven', 'tree', 'apple'];
 
-function chooseWords() {
+function chooseWord() {
 	let chosenWord = words[Math.floor(Math.random()*words.length)]
+	return chosenWord
 }
 
 
-game = {
-	//guesses remaining
-	guesses: 10,
-	//guessed letter
-	gussedLetters: [],
-	isOver() {
-		// Check if game has been won
+// const game = {
+// 	//guesses remaining
+// 	guesses: 10,
+// 	//guessed letter
+// 	gussedLetters: [],
+// 	isOver() {
+// 		// Check if game has been won
 
-		//Check if game has been lost
+// 		//Check if game has been lost
 
-		//if neither do this
-
-
-	},
-	overMessage() {
-		//If game has been won return "You Win"
-
-		//If game has been lost return "You Lose"
+// 		//if neither do this
 
 
-	}
+// 	},
+// 	overMessage() {
+// 		//If game has been won return "You Win"
 
-}
+// 		//If game has been lost return "You Lose"
+// 	}
+// };
 
 
-chooseWords()
+// class Letter {
+// 	constructor(value, hidden){
+// 		this.value = value;
+// 		this.hidden = true;
+// 	}
+// 	show() {
+// 		// Take a letter and change the value of hidden to false
+// 	}
+// 	display() {
+// 		//If the value if the letter is not hidden
+
+// 		//Return a _ if the letter is hidden
+// 	}
+// }
+
+
+// class Words {
+// 	constructor(newWord, letters){
+// 		this.letters = [];
+// 	}
+// 	getLetters(newWord) {
+// 		for(i = 0; i < newWord.length; i++){
+// 			let char = new Letter(newWord[i]);
+// 			this.letters.push(char);
+// 		}
+// 	}
+// 	isFound() {
+// 		// If true call game.overMessage()
+
+// 		//Otherwise return false
+// 	}
+// 	test(letter) {
+// 		//Go through letter array and update any hidden values that match this input
+// 	}
+// 	render() {
+// 		// Return the word showing either the unhidden letters (if guessed) or a _ if hidden
+// 	}
+// }
+
+
+
+
+
+
+
+
+chooseWord()
+
+
+
