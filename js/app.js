bbc658eaa9967aa70ec2c32f088c09d85ad9f8a9
Michael Weber <webermn15@gmail.com>
2017-12-20 20:34:46 -0600

    filled out Word and Letter class constructors with additional methods and logic

--- js/app.js
+++ js/app.js
@@ -32,14 +32,72 @@ const hangmanGame = {
 
 //a class for each word to be guessed. it contains methods to create Letter classes with each letter its made up of, as well as methods to check new letter guesses against existing letters and to render letters that have been revealed
 class Word {
-	//code
+	constructor(word, array) {
+		this.word = word;
+		this.array = [];
+	}
+	createLetters(randomWord) {
+		for (let i = 0; i < randomWord.length; i++) {
+			let letterObj = new Letter(randomWord[i]);
+			// console.log(letterObj);
+			this.array.push(letterObj);
+			// console.log(this.array);
+		}
+	}
 }
 
 
 //a class for each Letter of the word. this constructor is called for under a method in the Word class
 class Letter {
-	constructor(some parameters) {
-		this.whatever = yeahIllFigureItOutOkChill;
-		this.somethingElse = anotherThing;
+	//properties for each letter object, the letter itself contained in a string and a hidden property that defaults to true
+	constructor(letter) {
+		this.letter = letter;
+		this.hidden = true;
+	}
+	show() {
+		this.hidden = false;
+	}
+	display() {
+		if (this.hidden) {
+			return '_';
+		}
+		else {
+			return this.letter;
+		}
 	}
 }
+
+let boo = new Letter('l');
+console.log(boo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
