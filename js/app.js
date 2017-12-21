922b4f03c318b1ba9f8ee524b6276d0671846b66
Anthony <anthonyjlower@gmail.com>
2017-12-20 19:22:55 -0600

    Created word array and wrote function to select a random word

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// console.log("working")
+
+const words = ['hello', 'potato', 'seven', 'tree', 'apple'];
+
+function chooseWords() {
+	console.log(words[Math.floor(Math.random()*words.length)])
+}
+
+
+
+chooseWords()
